3cfa1336acd8e189273b9517be6157d16c715b53
bigsma11 <shenxyd@gmail.com>
2020-04-16 14:20:47 +0800

    fix the vh bug on mobile safari

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Chat() {
   return (
     <div>
-      <h1>Chat</h1>
+      <h1 style={{ color: 'white' }}>Chat</h1>
     </div>
   )
 }
--- client/src/components/join/Join.js
+++ client/src/components/join/Join.js
@@ -1,43 +1,41 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Join.module.css'
-import cx from 'classnames'
+import Div100vh from 'react-div-100vh'
 
 function Join() {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
 
   return (
-    <div className={styles.joinOuterContainer}>
-      <div className={styles.joinInnerContainer}>
-        <h1 className={styles.heading}>Join</h1>
-        <div>
-          <input
-            placeholder="Name"
-            className={styles.joinInput}
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
+    <Div100vh>
+      <div className={styles.joinOuterContainer}>
+        <div className={styles.joinInnerContainer}>
+          <h1 className={styles.heading}>Join</h1>
+          <form>
+            <input
+              placeholder="Name"
+              type="text"
+              className={styles.joinInput}
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+            <input
+              placeholder="Room"
+              type="text"
+              className={styles.joinInput}
+              onChange={(e) => setRoom(e.target.value)}
+              value={room}
+            />
+            <Link to={`/chat?name=${name}&room=${room}`}>
+              <button className={styles.button} type="submit" disabled={!name || !room}>
+                Sign In
+              </button>
+            </Link>
+          </form>
         </div>
-        <div>
-          <input
-            placeholder="Room"
-            className={cx(styles.joinInput, styles.mt20)}
-            onChange={(e) => setRoom(e.target.value)}
-            value={room}
-          />
-        </div>
-        <Link to={`/chat?name=${name}&room=${room}`}>
-          <button
-            className={cx(styles.button, styles.mt20)}
-            type="submit"
-            disabled={!name || !room}
-          >
-            Sign In
-          </button>
-        </Link>
       </div>
-    </div>
+    </Div100vh>
   )
 }
 
