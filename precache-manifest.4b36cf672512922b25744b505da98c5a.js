1f0bf6149f40e38e840716c6cec8254a214057e5
bigsma11 <shenxyd@gmail.com>
2020-04-17 23:51:58 +0800

    Updates

---
+++ precache-manifest.4b36cf672512922b25744b505da98c5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d241dd3a4eddbea57fc63a876f3c4679",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "427dc6a1165f4b689e17",
+    "url": "/chat-app/static/js/2.17d5f624.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app/static/js/2.17d5f624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8212b9bf38e119f5d8f",
+    "url": "/chat-app/static/js/main.5f63d4df.chunk.js"
+  },
+  {
+    "revision": "abb7b9782fca5cc4ce64",
+    "url": "/chat-app/static/js/runtime-main.375f3e37.js"
+  }
+]);
