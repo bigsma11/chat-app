a1516a756b7500d9d310eacfad05fc4dd9915770
bigsma11 <shenxyd@gmail.com>
2020-04-17 23:42:59 +0800

    deploy

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -19,7 +19,7 @@ function Chat() {
   const [message, setMessage] = useState('')
   const [messages, setMessages] = useState([])
 
-  const ENDPOINT = 'localhost:5000'
+  const ENDPOINT = 'https://react-chat-app-s.herokuapp.com/'
 
   // join the room
   useEffect(() => {
