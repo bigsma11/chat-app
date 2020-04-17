f720b5076810c9aee0c02fcb64b4bdd10c53e85e
bigsma11 <shenxyd@gmail.com>
2020-04-18 01:42:50 +0800

    fix samesite

--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   res.cookie('same-site-cookie', 'foo', { sameSite: 'lax' })
+  res.cookie('cross-site-cookie', 'bar', { sameSite: 'none', secure: true })
   res.send('server is up and running')
 })
 
