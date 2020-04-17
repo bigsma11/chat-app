e866d173061a8d297e944bf18d5bba6051563eba
bigsma11 <shenxyd@gmail.com>
2020-04-17 22:36:38 +0800

    deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const socketIO = require('socket.io')
 const http = require('http')
+const cors = require('cors')
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 const router = require('./router')
@@ -57,5 +58,6 @@ io.on('connection', (socket) => {
 })
 
 app.use(router)
+app.use(cors)
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
