6daf1d85adb69ca8609b1790833b4531738d03bc
bigsma11 <shenxyd@gmail.com>
2020-04-18 02:11:16 +0800

    fix samesite

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const socketIO = require('socket.io')
 const http = require('http')
 const cors = require('cors')
+const cp = require('cookie-parser')
 
 const { addUser, removeUser, getUser } = require('./users')
 const router = require('./router')
@@ -12,8 +13,36 @@ const app = express()
 const server = http.createServer(app)
 const io = socketIO(server)
 
+// handle cors
 app.use(cors())
 app.use(router)
+
+// handle SameSite cookie error
+app.use(cp())
+
+app.get('/set', (req, res) => {
+  // Set the new style cookie
+  res.cookie('myCookie', 'value', { sameSite: 'none', secure: true })
+  // And set the same value in the legacy cookie
+  res.cookie('myCookie-legacy', 'value', { secure: true })
+  res.end()
+})
+
+app.get('/', (req, res) => {
+  // eslint-disable-next-line no-unused-vars
+  let cookieVal = null
+
+  if (req.cookies['myCookie']) {
+    // check the new style cookie first
+    cookieVal = req.cookies['myCookie']
+  } else if (req.cookies['myCookie-legacy']) {
+    // otherwise fall back to the legacy cookie
+    cookieVal = req.cookies['myCookie-legacy']
+  }
+
+  res.end()
+})
+
 // manage user connect and disconnect
 io.on('connection', (socket) => {
   // join
--- server/router.js
+++ server/router.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.cookie('same-site-cookie', 'foo', { sameSite: 'lax' })
-  res.cookie('cross-site-cookie', 'bar', { sameSite: 'none', secure: true })
   res.send('server is up and running')
 })
 
