f43cd079becfd8258d948bc72402be44681bba2d
bigsma11 <shenxyd@gmail.com>
2020-04-18 19:32:33 +0800

    bug fix

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const socketIO = require('socket.io')
 const http = require('http')
 const cors = require('cors')
+const cp = require('cookie-parser')
 
 const { addUser, removeUser, getUser } = require('./users')
 const router = require('./router')
 
 // const PORT = process.env.PORT || 5000
-const PORT = 8000
+const PORT = 5000
 
 const app = express()
 const server = http.createServer(app)
@@ -16,6 +17,30 @@ const io = socketIO(server)
 // handle cors
 app.use(cors())
 app.use(router)
+
+app.use(cp())
+
+app.get('/set', (req, res) => {
+  // Set the new style cookie
+  res.cookie('3pcookie', 'value', { sameSite: 'none', secure: true })
+  // And set the same value in the legacy cookie
+  res.cookie('3pcookie-legacy', 'value', { secure: true })
+  res.end()
+})
+
+app.get('/', (req, res) => {
+  let cookieVal = null
+
+  if (req.cookies['3pcookie']) {
+    // check the new style cookie first
+    cookieVal = req.cookies['3pcookie']
+  } else if (req.cookies['3pcookie-legacy']) {
+    // otherwise fall back to the legacy cookie
+    cookieVal = req.cookies['3pcookie-legacy']
+  }
+
+  res.end()
+})
 
 // manage user connect and disconnect
 io.on('connection', (socket) => {
--- server/router.js
+++ server/router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/node', (req, res) => {
+router.get('/', (req, res) => {
   res.send('server is up and running')
 })
 
